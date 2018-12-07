470c5f55694b1e7af6d2ce5caa27c866b41cb794
Zoltan <zdebre@gmail.com>
2018-12-08 01:34:00 +1300

    Update package.json scripts

--- src/adapters/legacy-adapter.ts
+++ src/adapters/legacy-adapter.ts
@@ -8,12 +8,12 @@ export default class LegacyAdapter extends BaseAdapter {
     super();
     this.tracker = new LegacyTracker();
 
-    this.tracker.clientId = "CLIENT_ID";
-    this.tracker.server = "http://localhost/tracker/";
+    this.tracker.clientId = 'CLIENT_ID';
+    this.tracker.server = 'http://localhost/tracker/';
   }
 
   public track(pageTrack: Track): void {
-    const { pageName = "not specified" } = pageTrack;
+    const { pageName = 'not specified' } = pageTrack;
 
     this.tracker.pageName = pageName;
     this.tracker.trackPage();
